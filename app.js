631a3f0b3c6a78ada5508a5d0e6b33221e4d782b
PT <trivedp18@gmail.com>
2025-04-15 16:16:40 -0400

    fixed app.js so test works

--- app.js
+++ app.js
@@ -6,4 +6,7 @@ app.get('/health', (req, res) => {
   res.status(200).send('OK');
 });
 
+const userRoutes = require('./routes/users');
+app.use('/api', userRoutes);
+
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -6,16 +6,4 @@ router.get('/users', (req, res) => {
     res.json(['List of users']);
 });
 
-router.post('/users', (req, res) => {
-    res.send("Adds user");
-});
-
-router.put('/users', (req, res) => {
-    res.send("Replaces existing information");
-});
-
-router.patch('/users', (req, res) => {
-    res.send("Updates email");
-});
-
 module.exports = router;
