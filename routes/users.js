c579fb463b84282f45e9bd2baa46c694ae6b36bd
PT <trivedp18@gmail.com>
2025-04-15 15:07:24 -0400

    routes in users.js

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+router.get('/users', (req, res) => {
+    res.json(['List of users']);
+});
+
+router.post('/users', (req, res) => {
+    res.send("Adds user");
+});
+
+router.put('/users', (req, res) => {
+    res.send("Replaces existing information");
+});
+
+router.patch('/users', (req, res) => {
+    res.send("Updates email");
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,7 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const usersRouter = require('./routes/users');
-const tenantsRouter = require('./routes/tenants');
+const app = require('./app');
 
-app.use('/users', usersRouter);
-app.use('/tenants', tenantsRouter);
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
