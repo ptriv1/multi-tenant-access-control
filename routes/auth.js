266766fafd22ce63f6e82d16334a4f220ae466c2
PT <trivedp18@gmail.com>
2025-04-17 18:42:49 -0400

    auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,16 @@
-// route /login
-// validate user input
-// check username and password
-// if matching, generate token
-// if not, return error
-// returns token
+const { verifyLogin } = require('./services/authService.js');
 
 const express = require('express');
 const router = express.Router();
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const token = await verifyLogin(username, password);
     console.log("POST /login was hit");
-    res.json(['Creates token']);
+    if (token)
+        res.status(200).json({ token });
+    else
+        res.status(401).json({ message: 'Invalid credentials' });
 });
+
+module.exports = router;
--- services/authService.js
+++ services/authService.js
@@ -1,9 +1,30 @@
-// checks if username is in database
-// checks if password is valid
-// checks if user is active
-// generates token
-// blocks login if deactivated
+const logins = [
+    {
+      "username": "amandajonson",
+      "password": "$2b$10$RB/cSehL2ovtPQXxQTy1JOF8H2DoOsXAsv4Ih5FOgkeYBTVy9Ip9m"
+    },
+    {
+      "username": "bobvenders",
+      "password": "$2b$10$on571Jsv2l5tjq93uaB0XOn/CcSZZjkK5vNacRee0WTXlhO9ulyLq"
+    }
+  ]
+  
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
-// if username is valid && password is valid
-// generate token
-// else block login
+
+async function verifyLogin(username, password) {
+    const user = logins.find(user => user.username === username);
+        if (!user)
+            return null;
+        else
+            if (await (bcrypt.compare(password, user.password))) {
+                const token = jwt.sign({ username }, 'secret', { expiresIn: '1h' });
+                return token;
+            }
+            else
+                return null;
+            
+};
+
+module.exports = { verifyLogin };
