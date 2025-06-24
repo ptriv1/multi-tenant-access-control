3f120e93ffdefc1f437cc557a7c722a115116d5a
PT <trivedp18@gmail.com>
2025-06-24 09:55:09 -0400

    to edit middleware.test.js

--- test/middleware.test.js
+++ test/middleware.test.js
@@ -20,15 +20,6 @@ describe('testing when user authentication with token fails', () => {
     });
 });
 
-describe('testing when tenant check fails', () => {
-    it('returns unauthorized message', async () => {
-        await request(app)
-            .get('/api/users')
-            .set('authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRqb2huc29uIiwidGVuYW50SWQiOjY1OCwicm9sZSI6ImFkbWluIiwiaWF0IjoxNzE0NTk0MDYyfQ.jPIyYKoosKy7AB_J-kRfWyg8O2Q1YB2RQacCja5dlco')
-            .expect(401);
-    });
-});
-
 describe('testing when role match succeeds', () => {
     it('returns successful login', async () => {
         await request(app)
@@ -37,6 +28,15 @@ describe('testing when role match succeeds', () => {
             .expect(200);
     })
 })
+
+describe('testing when tenant check fails', () => {
+    it('returns unauthorized message', async () => {
+        await request(app)
+            .get('/api/users')
+            .set('authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRqb2huc29uIiwidGVuYW50SWQiOjY1OCwicm9sZSI6ImFkbWluIiwiaWF0IjoxNzE0NTk0MDYyfQ.jPIyYKoosKy7AB_J-kRfWyg8O2Q1YB2RQacCja5dlco')
+            .expect(401);
+    });
+});
 
 describe('testing unsuccessful role match', () => {
     it('returns unauthorized message', async () => {
